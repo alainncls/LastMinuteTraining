53f83f52ef75935a21b3c6663df2ad8b3e9d9bd4
Romain <romain.balaine@epfedu.fr>
2014-10-11 14:32:11 +0200

    Ajout dependency mongodb + init connexion

--- src/fr/epf/lastminutetraining/dao/FormationDAO.java
+++ src/fr/epf/lastminutetraining/dao/FormationDAO.java
@@ -1,16 +1,26 @@
 package fr.epf.lastminutetraining.dao;
 
+import java.net.UnknownHostException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.jongo.Jongo;
+import org.jongo.MongoCollection;
+
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+
 import fr.epf.lastminutetraining.domain.Formation;
 
 public enum FormationDAO {
 	
 	INSTANCE;
+	public static final String DB_NAME = "test";
+    public static final String MONGO_HOST = "localhost";
+    public static final int MONGO_PORT = 27017;
 	
 	
 	public static FormationDAO getInstance(){
@@ -22,11 +32,28 @@ public enum FormationDAO {
 		
 	}
 	
+	public void init(){
+		try {
+            MongoClient mongo = new MongoClient(
+                    MONGO_HOST, MONGO_PORT);
+            DB db = mongo.getDB(DB_NAME);
+            
+            Jongo jongo = new Jongo(db);
+             
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+	
 	public void createFormation(Formation formation){
 		
 	}
 	
 	public void updateFormation(Formation formation){
+		
+	}
+	
+	public void deleteFormation(Formation formation){
 		
 	}
 }
