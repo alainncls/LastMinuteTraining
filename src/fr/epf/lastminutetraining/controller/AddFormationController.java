01ec372a8b6db15c49147eaea9f9735e2983f6df
Romain <romain.balaine@epfedu.fr>
2014-10-18 19:31:02 +0200

    Bidouillage avec le JSON de Paul

--- src/fr/epf/lastminutetraining/controller/AddFormationController.java
+++ src/fr/epf/lastminutetraining/controller/AddFormationController.java
@@ -29,14 +29,14 @@ public class AddFormationController extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		// Get data from form
-		String name = (String) req.getParameter("name");
-		int price = Integer.parseInt(req.getParameter("price"));
-		int priceLMT = Integer.parseInt(req.getParameter("priceLMT"));
-		String startDate = (String) req.getParameter("startDate");
-		String endDate = (String) req.getParameter("endDate");
-		int difficulty = Integer.parseInt(req.getParameter("difficulty"));
-		String description = (String) req.getParameter("description");
-		String prerequis = (String) req.getParameter("prerequis");
+		String name = (String) req.getParameter("name").replaceAll("\\u([0-9a-f]{3,4})","");
+		String price = (String) req.getParameter("price");
+		String priceLMT = "";//(int) (price - price*(0.2));//Integer.parseInt(req.getParameter("priceLMT"));
+		String startDate = (String) req.getParameter("dates.date").replaceAll("\\u([0-9a-f]{3,4})","");
+		String endDate = "";//(String) req.getParameter("endDate");
+		String difficulty = (String) req.getParameter("level").replaceAll("\\u([0-9a-f]{3,4})","");
+		String description = (String)req.getParameter("description");//(String) req.getParameter("description").replaceAll("\\u([0-9a-f]{3,4})","");
+		String prerequis = "";//(String) req.getParameter("prerequis");
 
 		Formation formation = Formation.builder().name(name).price(price)
 				.priceLMT(priceLMT).startDate(startDate).endDate(endDate)
