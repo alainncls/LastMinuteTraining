0f81de41aef75c5245e38dd4b92101619290b3bf
alainncls <alain.nicolas@epfedu.fr>
2014-10-27 22:03:55 +0100

    Tenté de mettre les étoiles

--- src/main/java/fr/epf/lastminutetraining/controller/NotFoundController.java
+++ src/main/java/fr/epf/lastminutetraining/controller/NotFoundController.java
@@ -1,20 +1,20 @@
 package fr.epf.lastminutetraining.controller;
 
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
 
 @Controller
 @RequestMapping("/404")
-public class NotFoundController extends HttpServlet {
+public class NotFoundController {
 
     @RequestMapping(method = RequestMethod.GET)
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
