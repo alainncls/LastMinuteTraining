d403dc1ad0c90f2b15983f27fafbb6c6bd36e90b
alainncls <alain.nicolas.perso@gmail.com>
2014-10-28 14:52:52 +0100

    Correctifs mineurs

--- src/main/java/fr/epf/lastminutetraining/controller/TrainingController.java
+++ src/main/java/fr/epf/lastminutetraining/controller/TrainingController.java
@@ -1,21 +1,14 @@
 package fr.epf.lastminutetraining.controller;
 
-import fr.epf.lastminutetraining.domain.Training;
-import fr.epf.lastminutetraining.service.TrainingDBService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.List;
+import fr.epf.lastminutetraining.domain.Training;
+import fr.epf.lastminutetraining.service.TrainingDBService;
 
 /**
  * Created by Baptiste on 28/10/2014.
