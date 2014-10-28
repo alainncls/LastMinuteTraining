58e6796d9ea232365c22250b7b037a4cbd137a9a
Baptiste14 <baptiste.vignal@epfedu.fr>
2014-10-28 15:24:02 +0100

    My account, 404 et work controller springifier

--- src/main/java/fr/epf/lastminutetraining/controller/WorkController.java
+++ src/main/java/fr/epf/lastminutetraining/controller/WorkController.java
@@ -3,6 +3,7 @@ package fr.epf.lastminutetraining.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,24 +13,7 @@ import java.io.IOException;
 
 
 @Controller
-@RequestMapping("/work")
 public class WorkController {
-    @RequestMapping(method = RequestMethod.GET)
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-
-        // Get the dispatcher JSP
-        RequestDispatcher dispatcher = req
-                .getRequestDispatcher("/WEB-INF/work.jsp");
-
-        // Forward the request
-        dispatcher.forward(req, resp);
-    }
-
-    @RequestMapping(method = RequestMethod.POST)
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-
-        doGet(req, resp);
-    }
+    @RequestMapping(method = RequestMethod.GET, value = "/work")
+    protected ModelAndView home(){ return new ModelAndView("work"); }
 }
