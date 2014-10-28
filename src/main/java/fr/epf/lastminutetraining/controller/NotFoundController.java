2b6b2d4bfa4667aebb0b1d6b6e9b848b97576c19
Baptiste14 <baptiste.vignal@epfedu.fr>
2014-10-28 15:24:02 +0100

    My account, 404 et work controller springifier

--- src/main/java/fr/epf/lastminutetraining/controller/NotFoundController.java
+++ src/main/java/fr/epf/lastminutetraining/controller/NotFoundController.java
@@ -10,29 +10,11 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
-@RequestMapping("/404")
 public class NotFoundController {
-
-    @RequestMapping(method = RequestMethod.GET)
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-
-        // Get the dispatcher JSP
-        RequestDispatcher dispatcher = req
-                .getRequestDispatcher("/WEB-INF/404.jsp");
-
-        // Forward the request
-        dispatcher.forward(req, resp);
-    }
-
-    @RequestMapping(method = RequestMethod.POST)
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-
-        doGet(req, resp);
-    }
-
+    @RequestMapping(method = RequestMethod.GET, value = "/404")
+    protected ModelAndView home(){ return new ModelAndView("404"); }
 }
