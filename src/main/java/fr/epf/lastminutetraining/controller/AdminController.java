391f4452c0cf4f6e5f0d687699c7d0c2fd33b846
Baptiste14 <baptiste.vignal@epfedu.fr>
2014-12-04 22:54:43 +0100

    Fix admin

--- src/main/java/fr/epf/lastminutetraining/controller/AdminController.java
+++ src/main/java/fr/epf/lastminutetraining/controller/AdminController.java
@@ -12,36 +12,37 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping(value = "/admin")
 public class AdminController {
 
 	@Autowired
 	private TrainingDBService service;
 
-	@RequestMapping(method = RequestMethod.GET, value = "/admin")
+	@RequestMapping(method = RequestMethod.GET, value = "/")
 	protected ModelAndView admin() {
 		return new ModelAndView("/admin");
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = "/admin/trainings")
+	@RequestMapping(method = RequestMethod.GET, value = "/trainings")
 	protected ModelAndView trainings() {
-		return new ModelAndView("trainingsAdmin", "trainings",
+		return new ModelAndView("/trainingsAdmin", "trainings",
 				service.findLastTraining());
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = "/admin/editTraining/{code}")
+	@RequestMapping(method = RequestMethod.GET, value = "/editTraining/{code}")
 	protected ModelAndView edit(@PathVariable("code") String code) {
 		return new ModelAndView("/editTraining", "training",
 				service.findTraining(code));
 	}
-    @RequestMapping(method = RequestMethod.POST, value = "/admin/editTraining/{code}")
-    protected ModelAndView login(@RequestParam(value = "training") Training training) {
+    @RequestMapping(method = RequestMethod.POST, value = "/editTraining/{code}")
+    protected ModelAndView edit(@RequestParam(value = "training") Training training) {
             return new ModelAndView("/admin");
     }
 
-	@RequestMapping(method = RequestMethod.GET, value = "/admin/deleteTraining/{code}")
+	@RequestMapping(method = RequestMethod.GET, value = "/deleteTraining/{code}")
 	protected ModelAndView delete(@PathVariable("code") ObjectId code) {
 		service.remove(code);
-        return new ModelAndView("redirect:/admin/trainings", "trainings",
+        return new ModelAndView("redirect:/trainings", "trainings",
 				service.findLastTraining());
 	}
 }
